cb5b2b74bd1a222c78f816640fd67d84bd9e4bf8
Killian-Aidalinfo <killian.stein@aidalinfo.fr>
2025-10-02 17:00:52 +0200

    fix: export type

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,3 +3,4 @@ export * from "./workflows/index.js";
 export * from "./shared/utils/TChunk/index.js";
 export { scaleway } from "./shared/utils/provider/scaleway.js";
 export { tool } from "ai";
+export type { ToolSet } from 'ai';
