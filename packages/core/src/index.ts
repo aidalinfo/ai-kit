078057791bf78fde873468e4c96e424dcb526862
Killian-Aidalinfo <killian.stein@aidalinfo.fr>
2025-10-02 15:52:57 +0200

    export tool ai sdk

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,3 +2,4 @@ export * from "./agents/index.js";
 export * from "./workflows/index.js";
 export * from "./shared/utils/TChunk/index.js";
 export { scaleway } from "./shared/utils/provider/scaleway.js";
+export { tool } from "ai";
